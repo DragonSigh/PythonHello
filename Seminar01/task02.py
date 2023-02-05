1c11a9f48e0463e7656431e36cb6edb2f9170d0c
DragonSigh <dragonsigh@gmail.com>
2023-02-05 11:49:55 +0300

    reverse task 2 from seminar 1

--- Seminar01/task02.py
+++ Seminar01/task02.py
@@ -1,12 +1,18 @@
-num = int(input())
-digit4 = (num % 10 ** 1) // 10 ** 0
-digit3 = (num % 10 ** 2) // 10 ** 1
-digit2 = (num % 10 ** 3) // 10 ** 2
-digit1 = (num % 10 ** 4) // 10 ** 3
+# ЗАДАЧА №2
 
-print(digit1, digit2, digit3)
+# Найдите сумму цифр трехзначного числа.
 
-print(f'Цифра в позиции тысяч равна {digit1}')
-print(f'Цифра в позиции сотен равна {digit2}')
-print(f'Цифра в позиции десятков равна {digit3}')
-print(f'Цифра в позиции единиц равна {digit4}')
+# 123 -> 6 (1 + 2 + 3)
+# 100 -> 1 (1 + 0 + 0)
+
+number = int(input("Введите трёхзначное число: "))
+n = number
+digit = 0
+result = 0
+
+while n != 0:
+    digit = n % 10
+    result = result + digit
+    n = n // 10
+
+print(number, "->", result)
